6c2c2426f05861aeb26e08cab1ffffbd29341164
SEHOLESM <seholesihle@gmail.com>
2025-06-10 18:17:13 +0200

    added-js
    added javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+// referencing our display element
+const display = document.getElementById('display');
+
+// track if we have performed a calculation
+let justCalculated = false
+function appendToDisplay(Value) {
+  console.log('but')
+  
+}
